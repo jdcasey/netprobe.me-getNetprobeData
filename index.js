43f28d317ce00a167bc40e69b61174940ea63358
John Casey <jdcasey@commonjava.org>
2020-12-26 17:05:13 -0600

    switch to promise-based retrieval and use moment.js for start/end

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Firestore = require('@google-cloud/firestore');
 const escapeHtml = require('escape-html');
+const moment = require('moment');
 
 // const PROJECTID = 'test-project-285722';
 const db = new Firestore();
@@ -27,30 +28,39 @@ exports.getNetprobeData = async(req, res) => {
 
   let start = escapeHtml(req.query.start);
   let end = escapeHtml(req.query.end);
-  if(!end){
-    end = new Date();
+
+  endDate = new Date();
+  if(end){
+    endDate = moment(end).toDate();
   }
 
-  if(!start){
-    start = new Date();
-    start.setHours(end.getHours()-6);
+  let startDate = new Date()
+  start.setHours(end.getHours()-6);
+
+  if(start){
+    startDate = moment(start).toDate();
   }
 
-  console.log(`Retrieving ${ds} results for node: ${node}`);
-  const snapshot = await db.collection(`netprobe/${node}/${ds}`)
-    .where(`tstamp <= ${end.getTime()}`).where(`tstamp >= ${start.getTime()}`)
+  console.log(`Retrieving ${ds} results for node: ${node} between ${startDate} and ${endDate}`);
+  db.collection(`netprobe/${node}/${ds}`)
+    .where(`tstamp <= ${endDate.getTime()}`).where(`tstamp >= ${startDate.getTime()}`)
     .orderBy('tstamp')
     .limit(400)
-    .get();
+    .get().then(docs=>{
+      let data = [];
+      docs.forEach(doc => {
+        let record = doc.data();
+        let ts = new Date(record.tstamp * 1000);
+        record.tstamp = ts;
+        record.tstr = ts.toLocaleTimeString();
+        data.push(record);
+      });
 
-  let data = [];
-  snapshot.forEach(doc => {
-    let record = doc.data();
-    let ts = new Date(record.tstamp * 1000);
-    record.tstamp = ts;
-    record.tstr = ts.toLocaleTimeString();
-    data.push(record);
-  });
+      return res.status(200).send(data);
+    }).catch(err=>{
+      console.error(err);
+      return res.status(400).send({ error: 'Unable to retrieve', err });
 
-  return res.status(200).send(data);
+    })
+
 };
